48062cd13f77e37b2f9b14a0a7da945d58b2188b
RammusXu <comte_ken@hotmail.com>
2015-07-17 07:51:49 +0000

    [rails101-ch9] View/Model Helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+    def render_post_content(post)
+        truncate(post.content, :length => 15)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 	belongs_to :group, counter_cache: :posts_count
 	belongs_to :author, class_name: "User", foreign_key: :user_id
 
+	scope :recent, -> { order("updated_at DESC") }
 	def editable_by?(user)
 		user && user == author
 	end
